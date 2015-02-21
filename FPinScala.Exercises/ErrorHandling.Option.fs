057d74cb287fad9362cef9f5ad83e7f5d88a89fb
Stephen Compall <scompall@nocandysw.com>
2015-02-21 13:34:38 -0500

    Add variance and traverse Option exercises.

--- FPinScala.Exercises/ErrorHandling.Option.fs
+++ FPinScala.Exercises/ErrorHandling.Option.fs
@@ -21,8 +21,19 @@ type 'a Option =
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Option =
+    let mean (xs: double list): double Option =
+        match xs with
+        | [] -> None
+        | _  -> Some (List.sum xs / double (List.length xs))
+
+    let variance (xs: double list): double Option =
+        failwith "todo"
+
     let map2 (a: 'a Option, b: 'b Option) (f: ('a * 'b) -> 'c): 'c Option =
         failwith "todo"
 
     let sequence (a: 'a Option list): 'a list Option =
+        failwith "todo"
+
+    let traverse (a: 'a list) (f: 'a -> 'b Option): 'b list Option =
         failwith "todo"
