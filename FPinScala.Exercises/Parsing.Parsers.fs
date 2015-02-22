77f3f43506d75d9925b7d7661c9482ce8fb209e6
Stephen Compall <scompall@nocandysw.com>
2015-02-22 14:50:23 -0500

    line, col, currentLine for Location.

--- FPinScala.Exercises/Parsing.Parsers.fs
+++ FPinScala.Exercises/Parsing.Parsers.fs
@@ -8,9 +8,12 @@ type 'a Parser =
 type Location =
     {Input: String; Offset: int}
 
-    member this.line = failwith "TODO"
+    member this.line =
+        (this.Input.Substring(0, this.Offset + 1).ToCharArray
+         |> Seq.ofArray |> Seq.filter (fun c -> c == '\n') |> Seq.length) + 1
 
-    member this.col = failwith "TODO"
+    member this.col =
+        this.Offset + 1 - this.Input.Substring(0, this.Offset + 1).LastIndexOf('\n')
 
     member this.toError (msg: string): ParseError =
         {Stack = [this, msg]; OtherFailures = []}
@@ -19,7 +22,8 @@ type Location =
         {Input = this.Input; Offset = this.Offset + n}
 
     member this.currentLine: String =
-        if (this.Input.Length > 1) then failwith "TODO s11" 
+        if (this.Input.Length > 1)
+        then this.Input.Split('\n') |> Seq.nth (this.line - 1)
         else ""
 
 and ParseError = {Stack: (Location * String) list;
