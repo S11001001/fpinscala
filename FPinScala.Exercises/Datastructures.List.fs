d5f235583dca3b15ab453e1f0749a9ead7871247
Stephen Compall <scompall@nocandysw.com>
2015-02-22 14:36:49 -0500

    Rewrite List exercises in F#.

--- FPinScala.Exercises/Datastructures.List.fs
+++ FPinScala.Exercises/Datastructures.List.fs
@@ -1,65 +1,62 @@
-﻿package FPinScala.Exercises.Datastructures
+﻿namespace FPinScala.Exercises.Datastructures
 
-sealed trait List[+A] // `List` data type, parameterized on a type, `A`
-case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
-case class Cons[+A](head: A, tail: List[A]) extends List[A] // Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`, which may be `Nil` or another `Cons`.
+open System
 
-object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
-    case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
-  } 
+type 'a List = // `List` data type, parameterized on a type, `A`
+    | Nil      // A `List` data constructor representing the empty list
+    | Cons of 'a * 'a List // Another data constructor, representing nonempty lists. Note that it contains another `'a list`, which may be `Nil` or another `Cons`.
+
+    static member apply ([<ParamArray>] xs: 'a[]): 'a List = // Variadic member syntax
+        failwith "TODO s11"
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module List = // `List` companion module. Contains functions for creating and working with lists.
+    let rec sum (ints: int List): int = // `rec` declares a recursive function
+        match ints with // A function that uses pattern matching to add up a list of integers
+        | Nil -> 0 // The sum of the empty list is 0.
+        | Cons (x,xs) -> x + sum xs // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
   
-  def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
-    case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
-  }
+    let rec product (ds: double List): double = 
+        match ds with
+        | Nil -> 1.0
+        | Cons (0.0, _) -> 0.0
+        | Cons (x,xs) -> x * product xs
   
-  def apply[A](as: A*): List[A] = // Variadic function syntax
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
+    let x = match List.apply(1,2,3,4,5) with
+            | Cons(x, Cons(2, Cons(4, _))) -> x
+            | Nil -> 42 
+            | Cons(x, Cons(y, Cons(3, Cons(4, _)))) -> x + y
+            | Cons(h, t) -> h + sum(t)
+            | _ -> 101 
 
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42 
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101 
-  }
+    let rec append (a1: 'a List, a2: 'a List): 'a List =
+        match a1 with
+        | Nil -> a2
+        | Cons(h, t) -> Cons (h, append (t, a2))
 
-  def append[A](a1: List[A], a2: List[A]): List[A] =
-    a1 match {
-      case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
-    }
-
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
-    }
+    let rec foldRight (elts: 'a List, z: 'b) (f: ('a * 'b) -> 'b): 'b = // Utility functions
+        match elts with
+        | Nil -> z
+        | Cons(x, xs) -> f (x, foldRight (xs, z) f)
   
-  def sum2(ns: List[Int]) = 
-    foldRight(ns, 0)((x,y) => x + y)
+    let sum2 (ns: int List) = 
+        foldRight (ns, 0) (fun (x,y) -> x + y)
   
-  def product2(ns: List[Double]) = 
-    foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+    let product2 (ns: double List) =
+        foldRight (ns, 1.0) (fun (x,y) -> x * y) // no shorthand syntax for lambdas exists
 
+    let tail (l: 'a List): 'a List = failwith "TODO"
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+    let setHead (l: 'a List, h: 'a): 'a List = failwith "TODO"
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+    let drop (l: 'a List, n: int): 'a List = failwith "TODO"
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+    let dropWhile (l: 'a List, f: 'a -> bool): 'a List = failwith "TODO"
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+    let init (l: 'a List): 'a List = failwith "TODO"
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+    let length (l: 'a List): int = failwith "TODO"
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+    let foldLeft (l: 'a List, z: 'b) (f: ('b * 'a) -> 'b): 'b = failwith "TODO"
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
-
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
-}
+    let map (l: 'a List) (f: 'a -> 'b): 'b List = failwith "TODO"
