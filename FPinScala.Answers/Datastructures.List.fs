fab59428ca3a9b4bc782a66ca691595b3a85d998
Stephen Compall <scompall@nocandysw.com>
2015-02-22 22:30:58 -0500

    Various List answers.

--- FPinScala.Answers/Datastructures.List.fs
+++ FPinScala.Answers/Datastructures.List.fs
@@ -1,62 +1,97 @@
-﻿namespace FPinScala.Exercises.Datastructures
+﻿namespace FPinScala.Answers.Datastructures
 
+open FPinScala.Exercises.Datastructures
 open System
 
-type 'a List = // `List` data type, parameterized on a type, `A`
-    | Nil      // A `List` data constructor representing the empty list
-    | Cons of 'a * 'a List // Another data constructor, representing nonempty lists. Note that it contains another `'a list`, which may be `Nil` or another `Cons`.
-
-    static member apply ([<ParamArray>] xs: 'a[]): 'a List = // Variadic member syntax
-        Array.foldBack (fun x xs -> Cons (x, xs)) xs Nil
-
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module List = // `List` companion module. Contains functions for creating and working with lists.
-    let rec sum (ints: int List): int = // `rec` declares a recursive function
-        match ints with // A function that uses pattern matching to add up a list of integers
-        | Nil -> 0 // The sum of the empty list is 0.
-        | Cons (x,xs) -> x + sum xs // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
-  
-    let rec product (ds: double List): double = 
-        match ds with
-        | Nil -> 1.0
-        | Cons (0.0, _) -> 0.0
-        | Cons (x,xs) -> x * product xs
-  
-    let x = match List.apply(1,2,3,4,5) with
-            | Cons(x, Cons(2, Cons(4, _))) -> x
-            | Nil -> 42 
-            | Cons(x, Cons(y, Cons(3, Cons(4, _)))) -> x + y
-            | Cons(h, t) -> h + sum(t)
-            | _ -> 101 
+module List2 = // `List` companion module. Contains functions for creating and working with lists.
 
-    let rec append (a1: 'a List) (a2: 'a List): 'a List =
-        match a1 with
-        | Nil -> a2
-        | Cons(h, t) -> Cons (h, append t a2)
+    // Although we could return `Nil` when the input list is empty, we
+    // choose to throw an exception instead. This is a somewhat
+    // subjective choice. In our experience, taking the tail of an
+    // empty list is often a bug, and silently returning a value just means
+    // this bug will be discovered later, further from the place where
+    // it was introduced.
+    //
+    // It's generally good practice when pattern matching to use `_`
+    // for any variables you don't intend to use on the right hand
+    // side of a pattern. This makes it clear the value isn't
+    // relevant.
+    let tail (l: 'a List): 'a List =
+        match l with
+        | Nil -> failwith "tail of empty list"
+        | Cons (_, t) -> t
 
-    let rec foldRight (elts: 'a List) (z: 'b) (f: 'a -> 'b -> 'b): 'b = // Utility functions
-        match elts with
+    // If a function body consists solely of a match expression, we'll
+    // often put the match on the same line as the function signature,
+    // rather than introducing another level of nesting.
+    let setHead (l: 'a List) (h: 'a): 'a List =
+        match l with
+        | Nil -> failwith "setHead on empty list"
+        | Cons (_, t) -> Cons (h, t)
+
+    // Again, it's somewhat subjective whether to throw an exception
+    // when asked to drop more elements than the list contains. The
+    // usual default for `drop` is not to throw an exception, since
+    // it's typically used in cases where this is not indicative of a
+    // programming error. If you pay attention to how you use `drop`,
+    // it's often in cases where the length of the input list is
+    // unknown, and the number of elements to be dropped is being
+    // computed from something else. If `drop` threw an exception,
+    // we'd have to first compute or check the length and only drop up
+    // to that many elements.
+    let rec drop (l: 'a List) (n: int): 'a List =
+        if (n <= 0) then l
+        else match l with
+             | Nil -> Nil
+             | Cons (_, t) -> drop t (n-1)
+
+    // Somewhat overkill, but to illustrate the feature we're using a
+    // _pattern guard_, to only match a `Cons` whose head satisfies
+    // our predicate, `f`. The syntax is to add `when <cond>` after the
+    // pattern, before the `->`, where `<cond>` can use any of the
+    // variables introduced by the pattern.
+    let rec dropWhile (l: 'a List) (f: 'a -> bool): 'a List =
+        match l with
+        | Cons (h, t) when f h -> dropWhile t f
+        | _ -> l
+
+    // Note that we're copying the entire list up until the last
+    // element. Besides being inefficient, the natural recursive
+    // solution will use a stack frame for each element of the list,
+    // which can lead to stack overflows for large lists (can you see
+    // why?). With lists, it's common to use a temporary, mutable
+    // buffer internal to the function (with lazy lists or streams,
+    // which we discuss in chapter 5, we don't normally do this). So
+    // long as the buffer is allocated internal to the function, the
+    // mutation is not observable and RT is preserved.
+    //
+    // Another common convention is to accumulate the output list in
+    // reverse order, then reverse it at the end, which doesn't
+    // require even local mutation. We'll write a reverse function
+    // later in this chapter.
+    let rec init (l: 'a List): 'a List =
+        match l with
+        | Nil -> failwith "init of empty list"
+        | Cons (_, Nil) -> Nil
+        | Cons (h, t) -> Cons (h, init t)
+
+    let init2 (l: 'a List): 'a List =
+        let buf = new System.Collections.Generic.List<'a>()
+        let rec go (cur: 'a List): 'a List = 
+            match cur with
+            | Nil -> failwith "init of empty list"
+            | Cons (_, Nil) -> List.apply(buf.ToArray())
+            | Cons (h, t) -> buf.Add h
+                             go(t)
+        go l
+
+    let length (l: 'a List): int =
+        List.foldRight l 0 (fun _ acc -> acc + 1)
+
+    let rec foldLeft (l: 'a List) (z: 'b) (f: 'b -> 'a -> 'b): 'b =
+        match l with
         | Nil -> z
-        | Cons(x, xs) -> f x (foldRight xs z f)
-  
-    let sum2 (ns: int List) = 
-        foldRight ns 0 (fun x y -> x + y)
-  
-    let product2 (ns: double List) =
-        foldRight ns 1.0 (fun x y -> x * y) // no shorthand syntax for lambdas exists
-
-    let tail (l: 'a List): 'a List = failwith "TODO"
-
-    let setHead (l: 'a List) (h: 'a): 'a List = failwith "TODO"
-
-    let drop (l: 'a List) (n: int): 'a List = failwith "TODO"
-
-    let dropWhile (l: 'a List) (f: 'a -> bool): 'a List = failwith "TODO"
-
-    let init (l: 'a List): 'a List = failwith "TODO"
-
-    let length (l: 'a List): int = failwith "TODO"
-
-    let foldLeft (l: 'a List) (z: 'b) (f: 'b -> 'a -> 'b): 'b = failwith "TODO"
+        | Cons (h, t) -> foldLeft t (f z h) f
 
     let map (l: 'a List) (f: 'a -> 'b): 'b List = failwith "TODO"
