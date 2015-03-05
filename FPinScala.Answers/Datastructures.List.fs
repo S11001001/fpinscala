8eb4ba00974bfabd0e999f4bb5ceca76ee71f329
Stephen Compall <scompall@nocandysw.com>
2015-03-04 21:36:01 -0500

    copy scala List answers through doubleToString

--- FPinScala.Answers/Datastructures.List.fs
+++ FPinScala.Answers/Datastructures.List.fs
@@ -126,4 +126,26 @@ module List2 = // `List` companion module. Contains functions for creating and w
     let foldLeftViaFoldRight (l: 'a List) (z: 'b) (f: 'b -> 'a -> 'b): 'b =
       List.foldRight l (fun b -> b) (fun a g b -> g (f b a)) z
 
+(*
+  /*
+  `append` simply replaces the `Nil` constructor of the first list with the second list, which is exactly the operation performed by `foldRight`.
+  */
+  def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] =
+    foldRight(l, r)(Cons(_,_))
+
+  /*
+  Since `append` takes time proportional to its first argument, and this first argument never grows because of the right-associativity of `foldRight`, this function is linear in the total length of all lists. You may want to try tracing the execution of the implementation on paper to convince yourself that this works.
+
+  Note that we're simply referencing the `append` function, without writing something like `(x,y) => append(x,y)` or `append(_,_)`. In Scala there is a rather arbitrary distinction between functions defined as _methods_, which are introduced with the `def` keyword, and function values, which are the first-class objects we can pass to other functions, put in collections, and so on. This is a case where Scala lets us pretend the distinction doesn't exist. In other cases, you'll be forced to write `append _` (to convert a `def` to a function value) or even `(x: List[A], y: List[A]) => append(x,y)` if the function is polymorphic and the type arguments aren't known.
+  */
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, Nil:List[A])(append)
+
+  def add1(l: List[Int]): List[Int] =
+    foldRight(l, Nil:List[Int])((h,t) => Cons(h+1,t))
+
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil:List[String])((h,t) => Cons(h.toString,t))
+*)
+
     let map (l: 'a List) (f: 'a -> 'b): 'b List = failwith "TODO"
